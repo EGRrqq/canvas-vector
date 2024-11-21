fad53a1f8350ed62557fa66a7110f0485905e6e5
EGRrqq <egrrqqdev@gmail.com>
2024-11-21 12:37:14 -0500

    feat(board): setBg depends on canvas size

--- src/board/settings/setBg.js
+++ src/board/settings/setBg.js
@@ -1,14 +1,18 @@
+import { Ctx } from "../../ctx/index.js";
 import { Draw } from "../../draw/index.js";
 
 /** @type {(color: import("./Settings.js").ISettings["bgColor"]) => void} */
 export const setBg = (color) => {
-	Draw.rect(
-		{
-			rect: {
-				position: { x: 0, y: 0 },
-				size: { width: window.innerWidth * 2, height: window.innerHeight * 2 },
-			},
-		},
-		{ fillStyle: color },
-	);
+  const { width, height } = Ctx.getCtx().canvas;
+
+  Draw.rect(
+    {
+      rect: {
+        position: { x: 0, y: 0 },
+        size: { width: width * 2, height: height * 2 },
+      },
+    },
+    { fillStyle: color }
+  );
 };
+
