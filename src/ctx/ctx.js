65745a5b79863ccf6856453c4d2004ebb1864d54
EGRrqq <egrrqqdev@gmail.com>
2024-11-21 10:33:51 -0500

    feat(ctx): context controller

---
+++ src/ctx/ctx.js
@@ -0,0 +1,36 @@
+/** @typedef{() => CanvasRenderingContext2D} TGetContext*/
+/** @typedef{(id: number, settings?: ICtxSettings) => {getCtx: TGetContext}} TSetContext*/
+
+/**
+ * @typedef {object} ICtxSettings
+ * @property {boolean} alpha
+ */
+
+/** @type {CanvasRenderingContext2D | null} */
+let CTX = null;
+
+/** @type {ICtxSettings} */
+const defaultSettings = {
+	alpha: false,
+};
+
+/** @type {TSetContext} */
+const setCtx = (id, settings) => {
+	const s = { ...defaultSettings, ...settings };
+
+	const canvas = document.getElementById(id);
+
+	if (!(canvas instanceof HTMLCanvasElement))
+		throw new Error(`canvas елемент c id: '${id}' не найден`);
+
+	CTX = canvas.getContext("2d", { alpha: s.alpha });
+
+	return { getCtx };
+};
+
+/** @type {TGetContext} */
+const getCtx = () => {
+	if (!CTX) throw new Error("Ошибка во время получения 2д контекста");
+
+	return CTX;
+};
