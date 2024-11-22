13d661e45ddb573239917d059d38c66cf2686f13
EGRrqq <egrrqqdev@gmail.com>
2024-11-22 02:21:18 -0500

    fix(board): go back to window width

--- src/board/settings/setBg.js
+++ src/board/settings/setBg.js
@@ -1,18 +1,16 @@
-import { Ctx } from "../../ctx/index.js";
 import { Draw } from "../../draw/index.js";
 
 /** @type {(color: import("./Settings.js").ISettingsProps["bgColor"]) => void} */
 export const setBg = (color) => {
-  const { width, height } = Ctx.getCtx().canvas;
+	const { innerWidth: width, innerHeight: height } = window;
 
-  Draw.rect(
-    {
-      rect: {
-        position: { x: 0, y: 0 },
-        size: { width: width * 2, height: height * 2 },
-      },
-    },
-    { fillStyle: color }
-  );
+	Draw.rect(
+		{
+			rect: {
+				position: { x: 0, y: 0 },
+				size: { width: width * 2, height: height * 2 },
+			},
+		},
+		{ fillStyle: color },
+	);
 };
-
