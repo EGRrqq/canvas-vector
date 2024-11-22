6ca3534feddf585e1322c1a80fa3096421d42536
EGRrqq <egrrqqdev@gmail.com>
2024-11-22 09:10:43 -0500

    feat: magnit cursor on hover

--- src/toolbox/tool/room/room.js
+++ src/toolbox/tool/room/room.js
@@ -33,11 +33,28 @@ const roomHover = () => {
 			Draw.rect({ rect: { position: p, size: size } }, { fillStyle });
 		}
 
-		// Рисуем линию между точками
-		if (points.length) Draw.line({ path: points, mousePosition });
+		if (points.length) {
+			const magnetPoint = getMagnetPoint(points, mousePosition);
 
-		// Рисуем квадрат на ховере
-		Draw.rect({ rect: { position: mousePosition, size: size } }, { fillStyle });
+			// Проверка на магнитную точку
+			if (magnetPoint) {
+				// рисуем линию и квадрат к магнитной точке
+				Draw.line({ path: points, mousePosition: magnetPoint });
+
+				Draw.rect(
+					{ rect: { position: magnetPoint, size: size } },
+					{ fillStyle: "blue" },
+				);
+			} else {
+				// рисуем линию между точками и квадрат на ховере
+				Draw.line({ path: points, mousePosition });
+
+				Draw.rect(
+					{ rect: { position: mousePosition, size: size } },
+					{ fillStyle },
+				);
+			}
+		}
 	}
 
 	return { ...Methods, roomClick };
@@ -56,6 +73,17 @@ const roomClick = () => {
 	}
 
 	return { ...Methods, roomHover };
+};
+
+/** @type {(points: import("../../../models/base/IPoint.js").IPoint[], mousePosition: import("../../../models/base/IPoint.js").IPoint) =>  import("../../../models/base/IPoint.js").IPoint | null} */
+const getMagnetPoint = (points, mousePos) => {
+	if (points.length > 2) {
+		const point = points[0];
+		if (Math.hypot(point.x - mousePos.x, point.y - mousePos.y) < 8) {
+			return point; // Возвращаем точку, к которой магнитится курсор
+		}
+	}
+	return null; // Если магнитных точек нет
 };
 
 /** @type {IRoom} */
