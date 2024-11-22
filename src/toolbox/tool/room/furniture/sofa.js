bd393bfda873102409a43c2c8f40bd6ede531ba5
EGRrqq <egrrqqdev@gmail.com>
2024-11-22 15:25:30 -0500

    feat(furniture): detect walls

--- src/toolbox/tool/room/furniture/sofa.js
+++ src/toolbox/tool/room/furniture/sofa.js
@@ -33,10 +33,25 @@ export const stopDraggingSofa = () => {
 	isDraggingSofa = false;
 };
 
+// ---------------------------------
+
+/** @type {(points: import("../../../../models/base/IPoint.js").IPoint[]) => {start: import("../../../../models/base/IPoint.js").IPoint, end: import("../../../models/base/IPoint.js").IPoint}[]} */
+const getWalls = (points) => {
+	const walls = [];
+	for (let i = 0; i < points.length; i++) {
+		const start = points[i];
+		const end = points[(i + 1) % points.length];
+		walls.push({ start, end });
+	}
+	return walls;
+};
+
 /** @type {(sofaPos: import("../../../../models/base/IPoint").IPoint, wallPoint: import("../../../../models/base/IPoint").IPoint) => number} */
 const getSofaRotationAngle = (sofaPos, wallPoint) => {
-	const deltaX = wallPoint.x - sofaPos.x;
-	const deltaY = wallPoint.y - sofaPos.y;
+	const sofaBackX = sofaPos.x + sofaImage.width / 2; // Задняя часть дивана
+	const sofaBackY = sofaPos.y + sofaImage.height / 2; // Задняя часть дивана
+	const deltaX = wallPoint.x - sofaBackX;
+	const deltaY = wallPoint.y - sofaBackY;
 	return Math.atan2(deltaY, deltaX) * (180 / Math.PI); // Угол в градусах
 };
 
@@ -52,16 +67,52 @@ export const updateSofaPosition = (mousePos, points) => {
 			sofaPos = newSofaPosition;
 
 			// Проверяем магнитное примагничивание к стенам
-			for (const point of points) {
-				const distance = Math.hypot(point.x - sofaPos.x, point.y - sofaPos.y);
-				// Если диван близко к стене
-				if (distance < RADIUS) {
-					const angle = getSofaRotationAngle(sofaPos, point);
-					sofaRotationAngle = angle;
+			const walls = getWalls(points);
+			let closestDistance = RADIUS;
+			let closestWall = null;
 
-					break; // Прерываем цикл, если нашли стену
+			for (const wall of walls) {
+				const distance = pointToLineDistance(sofaPos, wall.start, wall.end);
+				if (distance < closestDistance) {
+					closestDistance = distance;
+					closestWall = wall;
 				}
+			}
+
+			if (closestWall) {
+				const angle = getSofaRotationAngle(sofaPos, closestWall.start);
+				sofaRotationAngle = angle;
 			}
 		}
 	}
+};
+
+/** @type {(point: import("../../../../models/base/IPoint.js").IPoint, start: import("../../../../models/base/IPoint.js").IPoint, end: import("../../../models/base/IPoint.js").IPoint) => number} */
+const pointToLineDistance = (point, start, end) => {
+	const A = point.x - start.x;
+	const B = point.y - start.y;
+	const C = end.x - start.x;
+	const D = end.y - start.y;
+
+	const dot = A * C + B * D;
+	const len_sq = C * C + D * D;
+	const param = len_sq !== 0 ? dot / len_sq : -1;
+
+	let closestX = 0;
+	let closestY = 0;
+
+	if (param < 0) {
+		closestX = start.x;
+		closestY = start.y;
+	} else if (param > 1) {
+		closestX = end.x;
+		closestY = end.y;
+	} else {
+		closestX = start.x + param * C;
+		closestY = start.y + param * D;
+	}
+
+	const dx = point.x - closestX;
+	const dy = point.y - closestY;
+	return Math.sqrt(dx * dx + dy * dy);
 };
